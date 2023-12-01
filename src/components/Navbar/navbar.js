8f47563497259b8b6857b3f91dec0674d7920cd2
Omer <shake.omer@gmail.com>
2023-12-01 16:19:14 +1100

    mobile support

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,23 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
-import { Link } from "react-scroll";
 import contact from "../../assets/contact.png";
+import menu from "../../assets/menu.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <div className="desktopMenu">
-        <Link className="listItem">Home</Link>
-        <Link className="listItem">About</Link>
-        <Link className="listItem">Projects</Link>
-        <Link className="listItem">Portfolio</Link>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItem"
+        >
+          Projects
+        </Link>
       </div>
-      <button className="desktopMenuBtn">
+      <button
+        className="desktopMenuBtn"
+        onClick={() => {
+          document
+            .getElementById("contact")
+            .scrollIntoView({ behavior: "smooth" });
+        }}
+      >
         <img src={contact} alt="" className="desktopMenuImg" />
         Contact Me
       </button>
+
+      <img
+        src={menu}
+        alt="Menu"
+        className="mobMenu"
+        onClick={() => setShowMenu(!showMenu)}
+      />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItemMob"
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItemMob"
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItemMob"
+          onClick={() => setShowMenu(false)}
+        >
+          Projects
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          className="listItemMob"
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
